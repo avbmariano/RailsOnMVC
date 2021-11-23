5de2f38af11c472f9a3559f2d90b65415f881a9e
AnahVi <ana.mariano@prevision.com.br>
2021-11-22 22:03:59 -0300

    Associacoes&ActiveRecord

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  root "articles4#index"
   get "/articles", to: "articles#index"
   get "/articles2", to: "articles2#index"
   get "/articles3", to: "articles3#index"
   get "/articles4", to: "articles4#index"
+  get "/users", to:"articles4#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20211119175838_create_articles.rb
+++ db/migrate/20211119175838_create_articles.rb
@@ -1,9 +1,14 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles do |t|
+    create_table :users do |t|
       t.string :title
       t.text :body
 
+      t.timestamps
+    end
+
+    create_table :articles do |t|
+      t.belongs_to :user
       t.timestamps
     end
   end
